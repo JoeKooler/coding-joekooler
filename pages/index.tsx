9d0129648989bb4d74268d01b12852c07406b499
JoeKooler <chitsanupong@pirsquare.net>
2022-12-06 12:41:20 +0700

    feat: add career imgs and add skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import twitterIcon from "../public/social_icons/mono_twitter.png";
 import Link from "next/link";
 import ProjectCard from "../components/ProjectCard/project_card";
 import ProjectCardMock from "../components/ProjectCard/project_card.mock";
+import CareerChart from "../public/career_img/career_chart.png";
+import CareerChartMobile from "../public/career_img/career_chart_mobile.png";
 
 // Todo
 // Finish Each section
@@ -38,7 +40,7 @@ export default function Home() {
             Resume
           </Link>
           <Link href="/" className="text-3xl z-10">
-            Contact
+            Projects
           </Link>
           <Link href="/" className="text-3xl z-10">
             About
@@ -71,7 +73,11 @@ export default function Home() {
           <div className="mt-6 md:mt-12" />
           <div className="px-14 justify-center md:justify-start flex md:px-0">
             <Link href={"https://github.com/JoeKooler"} className="mr-4">
-              <Image src={githubIcon} alt={"githubIcon"} className="duration-500 hover:invert"></Image>
+              <Image
+                src={githubIcon}
+                alt={"githubIcon"}
+                className="duration-500 hover:invert"
+              ></Image>
             </Link>
             <Link
               href={
@@ -79,17 +85,29 @@ export default function Home() {
               }
               className="mr-4"
             >
-              <Image src={linkedInIcon} alt={"linkedInIcon"} className="duration-500 hover:invert"></Image>
+              <Image
+                src={linkedInIcon}
+                alt={"linkedInIcon"}
+                className="duration-500 hover:invert"
+              ></Image>
             </Link>
             <Link href={"https://twitter.com/CodingJoekooler"} className="mr-4">
-              <Image src={twitterIcon} alt={"twitterIcon"} className="duration-500 hover:invert"></Image>
+              <Image
+                src={twitterIcon}
+                alt={"twitterIcon"}
+                className="duration-500 hover:invert"
+              ></Image>
             </Link>
 
             <Link
               href={"https://www.youtube.com/channel/UCfzx0Ga-7_tzLM-HSrBB67w"}
               className="mr-4"
             >
-              <Image src={youtubeIcon} alt={"youtubeIcon"} className="duration-500 hover:invert"></Image>
+              <Image
+                src={youtubeIcon}
+                alt={"youtubeIcon"}
+                className="duration-500 hover:invert"
+              ></Image>
             </Link>
           </div>
 
@@ -106,18 +124,100 @@ export default function Home() {
       </section>
 
       <section className="min-h-screen bg-backgroundGrey text-white px-6 md:px-[8.5rem]">
-        <div className="pt-20" />
+        <div className="pt-24" />
         <div className="w-full text-center">
           <b className="text-3xl md:text-5xl">Featured Projects</b>
         </div>
 
-        <div className="pt-16"></div>
+        <div className="pt-20"></div>
 
         <div className="flex flex-nowrap flex-shrink-0 overflow-x-auto">
           <ProjectCardMock />
           <ProjectCardMock />
           <ProjectCardMock />
         </div>
+
+        <div className="pt-16"></div>
+
+        <Link
+          href="/project"
+          className="mx-auto block border-solid border-white border-2 py-4 px-8 text-center"
+        >
+          <button>
+            <h1>More Projects</h1>
+          </button>
+        </Link>
+        <div className="pt-16"></div>
+      </section>
+
+      <section className="min-h-screen bg-backgroundGrey text-white">
+        <div className="pt-24"></div>
+        <b className="text-5xl text-center block mx-auto">Career Experience</b>
+        <div className="pt-28"></div>
+        <Image
+          src={CareerChart}
+          alt={"Career Chart Image"}
+          className="hidden md:block mx-auto"
+        ></Image>
+        <Image
+          src={CareerChartMobile}
+          alt={"Career Chart Mobile Image"}
+          className="w-screen md:hidden pl-9"
+        ></Image>
+        <div className="pt-24"></div>
+      </section>
+
+      <section className="min-h-screen bg-backgroundGrey text-white">
+        <div className="pt-24"></div>
+        <b className="text-5xl text-center block mx-auto">Skills</b>
+        <div className="pt-28"></div>
+        <div className="grid grid-rows-2 lg:grid-flow-col gap-10 px-12 md:px-36">
+          <div className="row-span-2 border-dashed border-2 pl-7 pb-7 relative">
+            <b className="absolute -top-6 left-2 bg-backgroundGrey text-2xl md:text-4xl">
+              {"Web Dev (Proficient)"}
+            </b>
+            <div className="pt-7"></div>
+            <ul className="list-disc pl-4">
+              <li className="text-xl md:text-2xl">HTML, CSS, JS, and TS</li>
+              <li className="text-xl md:text-2xl">
+                React, React Native,and NextJS
+              </li>
+              <li className="text-xl md:text-2xl">Tailwind,and MUI</li>
+              <li className="text-xl md:text-2xl">
+                NodeJS, ExpressJS, and NestJS
+              </li>
+              <li className="text-xl md:text-2xl">Socket.IO</li>
+              <li className="text-xl md:text-2xl">
+                AWS (Lightsail, Lambda, and IVS)
+              </li>
+            </ul>
+          </div>
+          <div className="border-dashed border-2 pl-7 pb-7 relative">
+            <b className="absolute -top-6 left-2 bg-backgroundGrey text-2xl md:text-4xl">
+              {"Web Dev (Basic)"}
+            </b>
+            <div className="pt-7"></div>
+            <ul className="list-disc pl-4">
+              <li className="text-xl md:text-2xl">Nginx, and Linux</li>
+              <li className="text-xl md:text-2xl">Postgres, and MongoDB</li>
+              <li className="text-xl md:text-2xl">Puppeteer</li>
+              <li className="text-xl md:text-2xl">VueJS</li>
+            </ul>
+          </div>
+
+          <div className="border-dashed border-2 pl-7 pb-7 relative">
+            <b className="absolute -top-6 left-2 bg-backgroundGrey text-2xl md:text-4xl">
+              {"Game Development"}
+            </b>
+            <div className="pt-7"></div>
+            <ul className="list-disc pl-4">
+              <li className="text-xl md:text-2xl">Unity C#, and UniRX </li>
+              <li className="text-xl md:text-2xl">Python pygame</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="pt-24"></div>
       </section>
     </div>
   );
