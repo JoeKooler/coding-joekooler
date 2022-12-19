2e3079a6feaef7e309520293c59a493813a97e6b
JoeKooler <jojesin@gmail.com>
2022-12-19 11:16:25 +0700

    dep: add storybook and config

---
+++ .storybook/main.js
@@ -0,0 +1,45 @@
+const path = require("path");
+module.exports = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    docsPage: true,
+  },
+  webpackFinal: (config) => {
+    /**
+     * Add support for alias-imports
+     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
+     */
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      "@": [path.resolve(__dirname, "../src/"), path.resolve(__dirname, "../")],
+    };
+
+    /**
+     * Fixes font import with /
+     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
+     */
+    config.resolve.roots = [
+      path.resolve(__dirname, "../public"),
+      "node_modules",
+    ];
+
+    return config;
+  },
+};
