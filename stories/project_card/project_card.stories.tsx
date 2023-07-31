2be4ea3fd0d3b341decb18e18c6746ec0dd126c3
JoeKooler <jojesin@gmail.com>
2023-07-31 14:44:13 +0700

    fix: storybook project got no id

--- stories/project_card/project_card.stories.tsx
+++ stories/project_card/project_card.stories.tsx
@@ -1,9 +1,17 @@
-import ProjectCard from "components/ProjectCard/project_card";
-import ProjectCardMock from "components/ProjectCard/project_card.mock";
+import ProjectCard from 'components/ProjectCard/project_card';
+import ProjectCardMock from 'components/ProjectCard/project_card.mock';
 export default {
-  title: "Project Card",
+  title: 'Project Card',
   component: ProjectCard,
 };
 
-export const Raw = () => <ProjectCard src={""} name={"Project Name"} description={"Project Description"} techStacks={["Stack_1" , "Stack_2"]}/>;
+export const Raw = () => (
+  <ProjectCard
+    src={''}
+    name={'Project Name'}
+    description={'Project Description'}
+    techStacks={['Stack_1', 'Stack_2']}
+    id={'Project Id'}
+  />
+);
 export const Mocked = () => <ProjectCardMock />;
